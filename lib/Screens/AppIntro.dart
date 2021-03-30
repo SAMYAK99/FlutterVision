f1effa8da84bb11ae338a64f6759ca69606c6c91
SAMYAK99 <singhsamyak199@gmail.com>
2021-03-30 23:29:31 +0530

    Added Splash Screen

---
+++ lib/Screens/AppIntro.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppIntro extends StatefulWidget {
+  @override
+  _AppIntroState createState() => _AppIntroState();
+}
+
+class _AppIntroState extends State<AppIntro> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
---
+++ lib/Screens/SplashScreen.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vision/Screens/AppIntro.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => AppIntro())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: 200),
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                'Flutter',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
+              ),
+              Text(
+                'Vision',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+        ));
+  }
+}
